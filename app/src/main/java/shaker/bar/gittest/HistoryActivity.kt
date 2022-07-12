9b2d8a3f7f85e5c2f46c66cd2b212f361ea9153d
minoo <minwoo@dailyshot.co>
2022-07-12 16:04:13 +0900

    2: HistoryActivity 추가

---
+++ app/src/main/java/shaker/bar/gittest/HistoryActivity.kt
@@ -0,0 +1,11 @@
+package shaker.bar.gittest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
