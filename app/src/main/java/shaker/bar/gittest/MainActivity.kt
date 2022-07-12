3f3cdcac7e7ea228f401e09e86dd788d4df8271b
minoo <minwoo@dailyshot.co>
2022-07-12 16:06:28 +0900

    2: HistoryActivity 연결
    - MainActivity 와 HistoryActivity 연결

--- app/src/main/java/shaker/bar/gittest/MainActivity.kt
+++ app/src/main/java/shaker/bar/gittest/MainActivity.kt
@@ -1,11 +1,17 @@
 package shaker.bar.gittest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_history).setOnClickListener {
+            startActivity(Intent(this, HistoryActivity::class.java))
+        }
     }
 }
